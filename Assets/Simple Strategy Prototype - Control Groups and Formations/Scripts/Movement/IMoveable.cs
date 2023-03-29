7d77427a1430b66f63ecd3f99e2c465e22e2e215
hassan404 <hassanali21212@gmail.com>
2023-03-29 14:37:48 +0500

    dll version of the project

--- Assets/Simple Strategy Prototype - Control Groups and Formations/Scripts/Movement/IMoveable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using UnityEngine;
-
-public interface IMoveable 
-{
-    public void Move(Vector3 target);
-    public void Teleport(Vector3 target);
-    public void Stop();
-}
